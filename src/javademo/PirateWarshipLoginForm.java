8b23140cab897cf771772aa967d5a359bc9c9833
Vuksa <nebojsa92vuksic@gmail.com>
2017-10-04 09:29:00 +0200

    Renamed JPanel variable.

--- src/javademo/PirateWarshipLoginForm.java
+++ src/javademo/PirateWarshipLoginForm.java
@@ -9,7 +9,7 @@ import static java.awt.EventQueue.invokeLater;
 
 public class PirateWarshipLoginForm extends JFrame {
 
-    private JPanel contentPane;
+    private JPanel contentPanel;
     private JTextField usernameTextField;
     private JTextField passwordTextField;
 
@@ -25,10 +25,6 @@ public class PirateWarshipLoginForm extends JFrame {
         });
     }
 
-    private boolean validateUser(String username, String password) {
-        return username.equals("user") && password.equals("user");
-    }
-
     /**
      * Create the frame.
      */
@@ -40,10 +36,10 @@ public class PirateWarshipLoginForm extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
 
-        contentPane = new JPanel();
-        contentPane.setBounds(new Rectangle(0, 0, 640, 400));
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(null);
+        contentPanel = new JPanel();
+        contentPanel.setBounds(new Rectangle(0, 0, 640, 400));
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPanel.setLayout(null);
 
         JButton loginBtn = new JButton("Login");
         loginBtn.addActionListener(e -> {
@@ -51,39 +47,39 @@ public class PirateWarshipLoginForm extends JFrame {
             String password = passwordTextField.getText();
 
             if (validateUser(username, password)) {
-                JOptionPane.showMessageDialog(contentPane, "Login successful.", "Information", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(contentPanel, "Login successful.", "Information", JOptionPane.INFORMATION_MESSAGE);
                 System.exit(0);
             } else {
-                JOptionPane.showMessageDialog(contentPane, "Wrong username or password.", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(contentPanel, "Wrong username or password.", "Error", JOptionPane.ERROR_MESSAGE);
                 Thread.interrupted();
             }
         });
         loginBtn.setBorder(new LineBorder(new Color(0, 0, 255), 1, true));
         loginBtn.setBounds(10, 211, 115, 23);
-        contentPane.add(loginBtn);
+        contentPanel.add(loginBtn);
 
         JButton registrationBtn = new JButton("Registration");
         registrationBtn.setBorder(new LineBorder(new Color(0, 0, 255), 1, true));
         registrationBtn.addActionListener(e -> {
-            JOptionPane.showMessageDialog(contentPane, "Registration screen not available.", "Information", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(contentPanel, "Registration screen not available.", "Information", JOptionPane.INFORMATION_MESSAGE);
             Thread.interrupted();
         });
         registrationBtn.setBounds(10, 247, 115, 23);
-        contentPane.add(registrationBtn);
+        contentPanel.add(registrationBtn);
 
         usernameTextField = new JTextField();
         usernameTextField.setFont(new Font("Arial Black", Font.BOLD, 11));
         usernameTextField.setOpaque(false);
         usernameTextField.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
         usernameTextField.setBounds(10, 91, 115, 20);
-        contentPane.add(usernameTextField);
+        contentPanel.add(usernameTextField);
         usernameTextField.setColumns(10);
 
         JLabel lblUser = new JLabel("User");
         lblUser.setFont(new Font("Ariel", Font.BOLD, 14));
         lblUser.setText("<html><font color='black'>User:</font></html>");
         lblUser.setBounds(10, 66, 46, 14);
-        contentPane.add(lblUser);
+        contentPanel.add(lblUser);
 
         passwordTextField = new JTextField();
         passwordTextField.setFont(new Font("Arial Black", Font.BOLD, 11));
@@ -91,14 +87,14 @@ public class PirateWarshipLoginForm extends JFrame {
         passwordTextField.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
         passwordTextField.setBounds(10, 137, 115, 20);
         passwordTextField.setColumns(10);
-        contentPane.add(passwordTextField);
+        contentPanel.add(passwordTextField);
 
         JLabel lblPassword = new JLabel("");
         lblPassword.setFont(new Font("Ariel", Font.BOLD, 14));
         lblPassword.setText("<html><font color='black'>" + "Password:" + "</font></html>");
         lblPassword.setBounds(10, 113, 80, 23);
 
-        contentPane.add(lblPassword);
+        contentPanel.add(lblPassword);
 
         JLabel background = new JLabel("");
         background.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -107,8 +103,12 @@ public class PirateWarshipLoginForm extends JFrame {
         ImageIcon icon = new ImageIcon(PirateWarshipLoginForm.class.getResource("../res/PirateShip.gif"));
         background.setIcon(icon);
 
-        contentPane.add(background);
+        contentPanel.add(background);
 
-        setContentPane(contentPane);
+        setContentPane(contentPanel);
+    }
+
+    private boolean validateUser(String username, String password) {
+        return username.equals("user") && password.equals("user");
     }
 }
